dd9eae1ae7ae18db9115627adf173a5df702f533
Aniket Ashok Raskar <aniketraskar932@gmail.com>
2025-07-16 13:51:16 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,36 +6,53 @@ function Projects() {
   return (
     <div className='pb-4'>
       <motion.h2
-      whileInView={{opacity:1 , y:0}}
-      initial={{opacity:0,y:-100}}
-      transition={{duration : 1.5}}
-      className='my-20 text-center text-4xl'>Projects</motion.h2>
-      <div>
-        {PROJECTS.map((project , index) =>(
-          <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
-          <motion.div 
-          whileInView={{opacity:1 ,x:0}}
-          initial={{opacity:0, x:100}}
-          transition={{duration:1}}
-          className='w-full lg:w-1/4'>
-            <img src={project.image} width={250} height={250} alt={project.title} className='mb-6 rounded-md'></img>
-          </motion.div>
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className='my-20 text-center text-4xl'>
+        Projects
+      </motion.h2>
 
-          <motion.div 
-          whileInView={{opacity:1 ,x:0}}
-          initial={{opacity:0, x:-100}}
-          transition={{duration:1}}
-          className='w-full max-w-xl lg:w-3/4'>
-            <h3 className='mb-2 font-semibold text-2xl'>
-            <a href='https://intellect-hire.vercel.app/' target="_blank" rel='noopener noreferrer' aria-label='Github'>
-              {project.title}
-            </a>
-            </h3>
-            <p className='mb-4 text-stone-400 '>{project.description}</p>
-            {project.technologies.map((tech , index)=>(
-                <span className='mr-2 my-5 rounded-md bg-stone-900 p-2 text-sm font-medium text-stone-300' key={index}>{tech}</span>
-            ))}
-          </motion.div>
+      <div>
+        {PROJECTS.map((project, index) => (
+          <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className='w-full lg:w-1/4'>
+              <img
+                src={project.image}
+                width={250}
+                height={250}
+                alt={project.title}
+                className='mb-6 rounded-md'
+              />
+            </motion.div>
+
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className='w-full max-w-xl lg:w-3/4'>
+              <h3 className='mb-2 font-semibold text-2xl'>
+                <a
+                  href={project.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label={project.title}>
+                  {project.title}
+                </a>
+              </h3>
+              <p className='mb-4 text-stone-400 '>{project.description}</p>
+              {project.technologies.map((tech, index) => (
+                <span
+                  className='mr-2 my-5 rounded-md bg-stone-900 p-2 text-sm font-medium text-stone-300'
+                  key={index}>
+                  {tech}
+                </span>
+              ))}
+            </motion.div>
           </div>
         ))}
       </div>
